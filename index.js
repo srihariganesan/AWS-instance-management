d5b8825a3755b639692f50f3918e37ba637790ea
srihariganesan <srihariganesan27@gmail.com>
2022-08-12 18:13:16 +0530

    small chnage on the backend js file

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const port = 3000;
 
 function home(req, res) {
     res.sendFile(__dirname   + "/index.html");
-    access_key = req.query.access_key_id;
-    secret_key = req.query.secret_key;
-    exec("serverless config credentials --provider aws --key " + access_key + " --secret " + secret_key);
-    console.log("serverless config credentials --provider aws --key " + access_key + " --secret " + secret_key)
+    // access_key = req.query.access_key_id;
+    // secret_key = req.query.secret_key;
+    // exec("serverless config credentials --provider aws --key " + access_key + " --secret " + secret_key);
+    // console.log("serverless config credentials --provider aws --key " + access_key + " --secret " + secret_key)
 }
 app.get("/", home)
 
